f97ec1ce36e33ed1455a68ea594c7614e3ec4563
Beyond <highoverseer2122@gmail.com>
2022-09-03 21:52:05 -0400

    Initial version

--- src/main/java/net/beyondredemption2122/voidascension/events/ModEvents.java
+++ src/main/java/net/beyondredemption2122/voidascension/events/ModEvents.java
@@ -1,13 +1,16 @@
 package net.beyondredemption2122.voidascension.events;
 
 import net.beyondredemption2122.voidascension.setup.customblocks.tileentities.screen.PedestalScreen;
+import net.beyondredemption2122.voidascension.setup.customblocks.tileentities.tileentityrenderer.PedestalTileEntityRenderer;
 import net.beyondredemption2122.voidascension.setup.init.ContainerTypesinit;
+import net.beyondredemption2122.voidascension.setup.init.TileEntityTypesInit;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.entity.EntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.beyondredemption2122.voidascension.VoidAscension;
 import net.beyondredemption2122.voidascension.setup.ModSpawnEggs;
@@ -30,6 +33,7 @@ public class ModEvents {
     public static void clientSetup(FMLClientSetupEvent event) {
         ScreenManager.register(ContainerTypesinit.PEDESTAL_CONTAINER_TYPE.get(), PedestalScreen::new);
 
+        ClientRegistry.bindTileEntityRenderer(TileEntityTypesInit.PEDESTAL_TILE_ENTITY_TYPE.get(), PedestalTileEntityRenderer::new);
     }
 
     @SubscribeEvent
