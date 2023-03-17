deb2545e9b59256d7b886b5ae26f6e011580f56c
dotso <highoverseer2122@gmail.com>
2023-03-17 08:50:42 -0400

    Update

--- src/main/java/net/beyondredemption2122/voidascension/setup/moditems/custom/ModArmorItem.java
+++ src/main/java/net/beyondredemption2122/voidascension/setup/moditems/custom/ModArmorItem.java
@@ -8,6 +8,7 @@ import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
@@ -18,14 +19,13 @@ import net.minecraftforge.fml.common.Mod;
 import java.util.Map;
 import java.util.Objects;
 
-@Mod.EventBusSubscriber(modid = VoidAscension.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModArmorItem extends ArmorItem {
     private static final Map<IArmorMaterial, Effect> MATERIAL_TO_EFFECT_MAP_1 =
             new ImmutableMap.Builder<IArmorMaterial, Effect>()
                     .put(ModArmorMaterial.VOID_RELIC, Effects.ABSORPTION)
                     .build();
 
-    private  static final Map<IArmorMaterial, Effect> MATERIAL_TO_EFFECT_MAP_2 =
+    private static final Map<IArmorMaterial, Effect> MATERIAL_TO_EFFECT_MAP_2 =
             new ImmutableMap.Builder<IArmorMaterial, Effect>()
                     .put(ModArmorMaterial.VOID_RELIC, Effects.DAMAGE_BOOST)
                     .build();
@@ -36,8 +36,8 @@ public class ModArmorItem extends ArmorItem {
 
     @Override
     public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
-        if(!world.isClientSide()) {
-            if(hasFullSuitOfArmorOn(player)) {
+        if (!world.isClientSide()) {
+            if (hasFullSuitOfArmorOn(player)) {
                 evaluateArmorEffects1(player);
                 evaluateArmorEffects2(player);
             }
@@ -51,11 +51,12 @@ public class ModArmorItem extends ArmorItem {
             IArmorMaterial mapArmorMaterial = entry.getKey();
             Effect mapStatusEffect = entry.getValue();
 
-            if(hasCorrectArmorOn(mapArmorMaterial, player)) {
+            if (hasCorrectArmorOn(mapArmorMaterial, player)) {
                 addStatusEffectForMaterial(player, mapArmorMaterial, mapStatusEffect);
             }
         }
     }
+
     private void evaluateArmorEffects2(PlayerEntity player) {
         for (Map.Entry<IArmorMaterial, Effect> entry : MATERIAL_TO_EFFECT_MAP_2.entrySet()) {
             IArmorMaterial mapArmorMaterial = entry.getKey();
@@ -70,28 +71,42 @@ public class ModArmorItem extends ArmorItem {
     private void addStatusEffectForMaterial(PlayerEntity player, IArmorMaterial mapArmorMaterial, Effect mapStatusEffect) {
         boolean hasPlayerEffect = !Objects.equals(player.getEffect(mapStatusEffect), null);
 
-        if(hasCorrectArmorOn(mapArmorMaterial, player) && !hasPlayerEffect) {
+        if (hasCorrectArmorOn(mapArmorMaterial, player) && !hasPlayerEffect) {
             player.addEffect(new EffectInstance(mapStatusEffect, 400));
         }
     }
 
     private boolean hasFullSuitOfArmorOn(PlayerEntity player) {
+//        System.out.println(player.inventory.armor.get(0).getItem().equals(Items.AIR));
+
+//        System.out.println(player.inventory.armor.get(0).getItem() instanceof ModArmorItem);
+
         ItemStack boots = player.inventory.getArmor(0);
         ItemStack leggings = player.inventory.getArmor(1);
         ItemStack breastplate = player.inventory.getArmor(2);
         ItemStack helmet = player.inventory.getArmor(3);
 
-        return !helmet.isEmpty() && !breastplate.isEmpty()
+
+//        return false;
+
+        return !Items.AIR() && !breastplate.isEmpty()
                 && !leggings.isEmpty() && !boots.isEmpty();
     }
 
     private boolean hasCorrectArmorOn(IArmorMaterial material, PlayerEntity player) {
-        ArmorItem boots = ((ArmorItem)player.inventory.getArmor(0).getItem());
-        ArmorItem leggings = ((ArmorItem)player.inventory.getArmor(1).getItem());
-        ArmorItem breastplate = ((ArmorItem)player.inventory.getArmor(2).getItem());
-        ArmorItem helmet = ((ArmorItem)player.inventory.getArmor(3).getItem());
+        for (ItemStack armorStack : player.inventory.armor) {
+            if (!(armorStack.getItem() instanceof ArmorItem)) {
+                return false;
+            }
+        }
 
-        return helmet.getMaterial() == material && breastplate.getMaterial() == material &&
-                leggings.getMaterial() == material && boots.getMaterial() == material;
+
+            ArmorItem boots = ((ArmorItem) player.inventory.getArmor(0).getItem());
+            ArmorItem leggings = ((ArmorItem) player.inventory.getArmor(1).getItem());
+            ArmorItem breastplate = ((ArmorItem) player.inventory.getArmor(2).getItem());
+            ArmorItem helmet = ((ArmorItem) player.inventory.getArmor(3).getItem());
+
+            return helmet.getMaterial() == material && breastplate.getMaterial() == material &&
+                    leggings.getMaterial() == material && boots.getMaterial() == material;
     }
 }
